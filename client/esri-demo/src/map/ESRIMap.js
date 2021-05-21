3d09dffa855855f590037156b29d2fb9a509ab52
Karthik Kumar <jkkr.1996@gmail.com>
2021-05-21 12:40:32 +0530

    Add University Graphics layer

--- client/esri-demo/src/map/ESRIMap.js
+++ client/esri-demo/src/map/ESRIMap.js
@@ -1,12 +1,48 @@
 import React, { useRef, useEffect } from "react";
 import MapView from "@arcgis/core/views/MapView";
 import Map from "@arcgis/core/Map";
+import Graphic from "@arcgis/core/Graphic";
+import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+import collegeData from "./collegeData.json";
 
-import './ESRIMap.css'
+import "./ESRIMap.css";
 
 function ESRIMap() {
-
   const mapDiv = useRef(null);
+
+  const addUniversity = (map) => {
+    let markerSymbol = {
+      type: "simple-marker", // autocasts as new SimpleMarkerSymbol()
+      color: [25, 0, 220],
+      outline: {
+        // autocasts as new SimpleLineSymbol()
+        color: [0, 255, 0],
+        width: 2,
+      },
+    };
+    let markerGraphics = [];
+    collegeData.forEach((university) => {
+      var point = {
+        type: "point", // autocasts as new Point()
+        longitude: university.Longitude,
+        latitude: university.Latitude,
+      };
+
+      let pointGraphic = new Graphic({
+        geometry: point,
+        symbol: markerSymbol,
+      });
+
+      markerGraphics.push(pointGraphic);
+    });
+
+    let graphicsLayer = new GraphicsLayer({
+      id: "university",
+      graphics: markerGraphics,
+    });
+
+    map.add(graphicsLayer);
+  };
 
   useEffect(() => {
     if (mapDiv.current) {
@@ -14,7 +50,7 @@ function ESRIMap() {
        * Initialize application
        */
 
-       let map = new Map({
+      let map = new Map({
         basemap: "hybrid",
         layers: [],
       });
@@ -25,7 +61,7 @@ function ESRIMap() {
         zoom: 3,
         map: map,
       });
-
+      addUniversity(map);
     }
   }, []);
 
