9464dfad9b6a72b9285220ff06d4e1fb4d0acfeb
Karthik Kumar <jkkr.1996@gmail.com>
2021-06-01 22:05:58 +0530

    Add hospital data and endpoint

--- node-server/app.ts
+++ node-server/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import collegeData from "./Data/collegeData.json";
+import hospitalData from "./Data/hospitalData.json";
 import cors from "cors";
 //get router
 
@@ -33,7 +34,10 @@ const pgClient = new Pool({
   password: pgPassword,
   port: pgPort,
 });
-let dbInitFlag = true;
+
+let dbCollegeInitFlag = false;
+let dbHospitalInitFlag = false;
+
 pgClient.on("error", () => console.log("Connection lost to Postgres"));
 
 pgClient.on("connect", (client) => {
@@ -42,7 +46,7 @@ pgClient.on("connect", (client) => {
       "CREATE TABLE IF NOT EXISTS CollegeData (PlaceName VARCHAR (100), Latitude NUMERIC, Longitude NUMERIC)"
     )
     .then((res) => {
-      if (dbInitFlag) {
+      if (!dbCollegeInitFlag) {
         collegeData.forEach((college) => {
           client
             .query(
@@ -50,14 +54,38 @@ pgClient.on("connect", (client) => {
             )
             .catch((err) => console.error(err));
         });
+        dbCollegeInitFlag = true;
       }
-      dbInitFlag = false;
+    })
+    .catch((err) => console.error(err));
+
+  client
+    .query(
+      "CREATE TABLE IF NOT EXISTS HospitalData(PlaceName VARCHAR (100), Latitude NUMERIC, Longitude NUMERIC)"
+    )
+    .then((res) => {
+      if (!dbHospitalInitFlag) {
+        hospitalData.forEach((hospital) => {
+          client
+            .query(
+              `INSERT INTO HospitalData(PlaceName, Latitude, Longitude) VALUES ('${hospital["Place Name"]}', '${hospital.Latitude}', '${hospital.Longitude}');`
+            )
+            .catch((err) => console.error(err));
+        });
+        dbHospitalInitFlag = true;
+      }
     })
     .catch((err) => console.error(err));
 });
 
 app.get("/colleges", async (req, res) => {
   const data = await pgClient.query("SELECT * FROM CollegeData");
+  console.log("data", data);
+  res.send(data.rows);
+});
+
+app.get("/hospitals", async (req, res) => {
+  const data = await pgClient.query("SELECT * FROM HospitalData");
   res.send(data.rows);
 });
 
