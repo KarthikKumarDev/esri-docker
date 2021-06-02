86d8c2612f54ad46b2a3e924b6d095eee5107ee8
Karthik Kumar <jkkr.1996@gmail.com>
2021-06-02 20:23:42 +0530

    Add Map Selector Radio Button

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import ESRIMap from './map/ESRIMap';
+import MapSelector from './selector/MapSelector';
 
 function App() {
   return (
     <div className="App">
+      <MapSelector/>
       <ESRIMap></ESRIMap>
     </div>
   );
---
+++ client/src/selector/MapSelector.js
@@ -0,0 +1,12 @@
+import "./MapSelector.css";
+
+function MapSelector() {
+  return (
+    <div className="radio-selector">
+      <input type="radio" name="rg1" /> College Data
+      <input type="radio" name="rg1" /> Hospital Data
+    </div>
+  );
+}
+
+export default MapSelector;
